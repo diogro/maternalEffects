b452c507e9a3bba5ac18ce952f5463cb1da961da
diogro <diogro@gmail.com>
2014-07-01 16:43:45 -0300

    full cromossome scan

--- run_maternal_effects_lme4.R
+++ run_maternal_effects_lme4.R
@@ -1,11 +1,12 @@
 source('./read_mouse_data.R')
 
 library(lme4)
+library(lmerTest)
 library(ggplot2)
+library(doMC)
 
-ind = mouse_phen_std$ID [1]
-locus = 1
-cromossome = 1
+registerDoMC(10)
+
 get_design = function(ind, locus, cromossome){
     gen_cols = paste0(c("A", "D", "I"), locus)
     mother = f3_pedigree[which(ind == f3_pedigree$ID), 'Dam']
@@ -21,11 +22,41 @@ get_design = function(ind, locus, cromossome){
     else if(all(genotypes == c(-1, 0,  0,  0, 1)))  design = c(-1, -1/2,  0,  0,  1/2,  0, -1/4)
     else if(all(genotypes == c( 0, 1,  1, -1, 0)))  design = c( 0,  1/2,  0, -1, -1/2,  0, -1/4)
     else if(all(genotypes == c(-1, 0,  0, -1, 0)))  design = c( 0, -1/2,  0,  0, -1/2, -1,  1/4)
-    else {print(genotypes);print(ind);print(mother);design = rep(NA, 7)}
-    names(design) = c("a_0", "d_0", "i_0", "a_m", "d_m", "c_m0", "dd_m0")
+    else                                            design = rep(NA, 7)
+    names(design) = paste(c("a_0", "d_0", "i_0", "a_m", "d_m", "c_m0", "dd_m0"), locus, sep = '_')
     return(design)
 }
-get_design(ind, locus, cromossome)
-current_mouse_gen = adply(mouse_phen_std$ID, 1, get_design, locus, cromossome)
-design_matrix
-na.omit(merge(mouse_phen_std, current_mouse_gen, by.x = 'ID', by.y = 'X1'))
+
+runCromossome <- function(cromossome){
+    current_mouse_gen = mouse_phen_std$ID
+    num_loci = (length(mouse_gen[[cromossome]])-1)/3
+    for(locus in 1:num_loci){
+        current_mouse_gen = cbind(current_mouse_gen, adply(mouse_phen_std$ID, 1, get_design, locus, cromossome)[,-1])
+    }
+    current_data = na.omit(merge(mouse_phen_std, current_mouse_gen, by.x = 'ID', by.y = 'current_mouse_gen'))
+    melt_data = melt(current_data, id.vars = names(current_data)[c(1:6, 14:length(names(current_data)))])
+
+    #ggplot(melt_data, aes(variable, value, color = SEX)) + geom_boxplot() + facet_wrap(~SEX)
+
+    null_formula = "value ~ 1 + (0 + variable|FAMILY)"
+    mouse_model_no_gen = lmer(as.formula(null_formula),
+                              data = melt_data,
+                              REML = FALSE)
+    G_lme4 = VarCorr(mouse_model_no_gen)[[1]]
+    attr(G_lme4,"correlation") = NULL
+
+    runSingleLocusModel <- function(locus, cromossome){
+        genotype_formula = paste(null_formula,
+                                 paste(paste('variable*',  paste(c("a_0", "d_0", "i_0", "a_m", "d_m", "c_m0", "dd_m0"), locus, sep = '_'), sep = ''), collapse = ' + '),
+                                 sep = ' + ')
+        mouse_model = lmer(as.formula(genotype_formula),
+                           data = melt_data,
+                           REML = FALSE)
+        test = anova(mouse_model_no_gen , mouse_model)
+        print(test)
+        return(list(model = mouse_model, anova = test, p.value = test$'Pr(>Chisq)'[2]))
+    }
+    cromossome_model_list = alply(1:num_loci, 1, runSingleLocusModel, cromossome, .parallel = TRUE)
+    return(cromossome_model_list)
+}
+maternal_scan = llply(names(mouse_gen), runCromossome)
