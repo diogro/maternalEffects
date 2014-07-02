f17fff0ac75463602656153cb6fe2c3a8adb7492
diogro <diogro@gmail.com>
2014-07-02 14:30:03 -0300

    running

--- read_mouse_data.R
+++ read_mouse_data.R
@@ -6,6 +6,9 @@ library(reshape2)
 raw_mouse_phen = read.csv("./data/F3phenotypes_uncorrected.csv", as.is = T)
 raw_mouse_phen = select(raw_mouse_phen, c(ID, FATPAD:PAIR))
 
+f3_pedigree = read.csv("./data/F3_pedigree.csv")
+f3_pedigree = f3_pedigree[-c(1997:2000),]
+
 raw_mouse_phen = mutate(raw_mouse_phen,
                           grow12 = WEEK2 - WEEK1,
                           grow23 = WEEK3 - WEEK2,
@@ -19,6 +22,9 @@ raw_mouse_meta = read.csv("./data/F3Phenotypes_further corrected family data_cor
 names(raw_mouse_meta) = gsub('SexAN', 'SEX', names(raw_mouse_meta))
 raw_mouse_meta = select(raw_mouse_meta, ID:COHORT)
 
+#selecting pups that where NOT cross-fostered
+raw_mouse_meta = raw_mouse_meta[raw_mouse_meta$NURSE == raw_mouse_meta$Dam,]
+
 raw_mouse_meta = raw_mouse_meta[raw_mouse_meta$ID %in% raw_mouse_phen$ID,]
 raw_mouse_phen = raw_mouse_phen[raw_mouse_phen$ID %in% raw_mouse_meta$ID,]
 
@@ -27,6 +33,11 @@ raw_mouse_gen = llply(paste0("./data/genotypes/chrom", 1:19, ".csv"), read.csv, 
 names(raw_mouse_gen) = paste0("chrom", 1:19)
 mouse_gen = raw_mouse_gen
 mouse_phen = mouse_phen[mouse_phen$ID %in% mouse_gen[[1]]$ID,]
+
+mouse_phen = mouse_phen[mouse_phen$ID %in% f3_pedigree$ID,]
+
+sum((na.omit(f3_pedigree[, 2]) %in% f3_pedigree[, 1]) == FALSE) > 0 & any(is.na(f3_pedigree[, 2]))
+sum((na.omit(f3_pedigree[, 3]) %in% f3_pedigree[, 1]) == FALSE) > 0 & any(is.na(f3_pedigree[, 3]))
 
 rm(list = ls(pattern='raw'))
 
