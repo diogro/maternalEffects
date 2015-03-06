5eff88fd02c82d4731d8b51ac336acd318f47dce
diogro <diogro@gmail.com>
2015-03-06 15:17:59 -0300

    MCMCglmm model

---
+++ run_maternal_effects_MCMCglmm.R
@@ -0,0 +1,88 @@
+source('./read_mouse_data.R')
+
+library(MCMCglmm)
+library(plyr)
+library(reshape2)
+library(lme4)
+library(lmerTest)
+library(ggplot2)
+library(doMC)
+
+registerDoMC(50)
+
+get_design = function(ind, locus, cromossome){
+    gen_cols = paste0(c("A", "D", "I"), locus)
+    mother = f3_pedigree[which(ind == f3_pedigree$ID), 'Dam']
+    mother_gen = mouse_gen[[cromossome]][mouse_gen[[cromossome]]$ID == mother, gen_cols[-3]]
+    names(mother_gen) = paste0("M", names(mother_gen))
+    ind_gen = mouse_gen[[cromossome]][mouse_gen[[cromossome]]$ID == ind, gen_cols]
+    genotypes = matrix(data.frame(ind_gen, mother_gen))
+    if     (all(genotypes == c( 1, 0,  0,  1, 0)))  design = c( 0, -1/2,  0,  0, -1/2,  1,  1/4)
+    else if(all(genotypes == c( 0, 1, -1,  1, 0)))  design = c( 0,  1/2,  0,  1, -1/2,  0, -1/4)
+    else if(all(genotypes == c( 1, 0,  0,  0, 1)))  design = c( 1, -1/2,  0,  0,  1/2,  0, -1/4)
+    else if(all(genotypes == c( 0, 1, -1,  0, 1)))  design = c( 0,  1/2, -1,  0,  1/2,  0,  1/4)
+    else if(all(genotypes == c( 0, 1,  1,  0, 1)))  design = c( 0,  1/2,  1,  0,  1/2,  0,  1/4)
+    else if(all(genotypes == c(-1, 0,  0,  0, 1)))  design = c(-1, -1/2,  0,  0,  1/2,  0, -1/4)
+    else if(all(genotypes == c( 0, 1,  1, -1, 0)))  design = c( 0,  1/2,  0, -1, -1/2,  0, -1/4)
+    else if(all(genotypes == c(-1, 0,  0, -1, 0)))  design = c( 0, -1/2,  0,  0, -1/2, -1,  1/4)
+    else                                            design = rep(NA, 7)
+    names(design) = paste(c("a_0", "d_0", "i_0", "a_m", "d_m", "c_m0", "dd_m0"), locus, sep = '_')
+    return(design)
+}
+
+runCromossome <- function(cromossome){
+    cromossome = 4
+
+    current_mouse_gen = mouse_phen_std$ID
+    num_loci = (length(mouse_gen[[cromossome]])-1)/3
+    for(locus in 1:num_loci){
+        current_mouse_gen = cbind(current_mouse_gen,
+                                  adply(mouse_phen_std$ID, 1, get_design, locus, cromossome)[,-1])
+    }
+    current_data = na.omit(merge(mouse_phen_std, current_mouse_gen, by.x = 'ID', by.y = 'current_mouse_gen'))
+    #melt_data = melt(current_data, id.vars = names(current_data)[c(1:6, 14:length(names(current_data)))])
+
+    #ggplot(melt_data, aes(variable, value, color = SEX)) + geom_boxplot() + facet_wrap(~SEX)
+
+    num.traits = 7
+    value = paste("cbind(",
+                  paste(paste("grow",
+                              paste(1:num.traits, 2:(num.traits+1), sep = ''),
+                              sep = ''), collapse = ', '),
+                  ")", sep = '')
+
+    fixed.effects = "trait:SEX + trait:LSB + trait:LSW + trait:COHORT - 1"
+
+    null_formula = paste(value, fixed.effects, sep = ' ~ ')
+
+    runSingleLocusMCMCModel <- function(locus){
+        genotype_formula = paste(null_formula,
+                                 paste(paste('trait:',
+                                             paste(c("a_0", "d_0", "i_0",
+                                                     "a_m", "d_m",
+                                                     "c_m0", "dd_m0"),
+                                                   locus,
+                                                   sep = '_'),
+                                             sep = ''),
+                                       collapse = ' + '),
+                                 sep = ' + ')
+        prior = list(R = list(V = diag(7), n = 0.002),
+                     G = list(G1 = list(V = diag(7) * 0.02, n = 8)))
+        mcmc.mouse.model = MCMCglmm(as.formula(genotype_formula),
+                                    random = ~us(trait):FAMILY,
+                                    data = current_data,
+                                    rcov = ~us(trait):units,
+                                    family = rep("gaussian", 7),
+                                    prior = prior,
+                                    verbose = FALSE)
+        return(mcmc.mouse.model)
+    }
+    cromossome_model_list = alply(1:num_loci, 1, runSingleLocusMCMCModel, .parallel = TRUE)
+    return(cromossome_model_list)
+}
+maternal_scan = llply(names(mouse_gen), runCromossome)
+names(maternal_scan) = names(mouse_gen)
+save(maternal_scan, file = "./Rdatas/maternalScan_MCMCglmm.Rdata")
+#load("./Rdatas/maternalScan_MCMCglmm.Rdata")
+
+
