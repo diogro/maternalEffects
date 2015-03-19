556b4a3d864d3ca02fa8a2f7214429731c04c9fe
diogro <diogro@gmail.com>
2015-03-19 17:05:39 -0300

    a n d

--- plot_maternal_effects_MCMCglmm.R
+++ plot_maternal_effects_MCMCglmm.R
@@ -5,8 +5,15 @@ model_summary <- ldply(maternal_scan_mcmc, function(x) ldply(x, getEffects, .par
 names(model_summary)[1:2] <- c('chrom', 'loci')
 model_summary$chrom <- factor(model_summary$chrom, levels = paste0('chrom', 1:19))
 
-x <- 'gr12'
-plots_trait <- llply(unique(model_summary$trait),
+plots_trait_a <- llply(unique(model_summary$trait),
+                     function(x) {
+                         ggplot(filter(model_summary, effect == 'a', trait == x), aes(loci, mean)) +
+                             facet_wrap(~chrom, scale = 'free_x') +
+                             geom_point() +
+                             geom_errorbar(aes(ymin = lower, ymax = upper)) + theme_bw() + geom_hline(h = 0) + theme(title = element_text(x))
+                     })
+
+plots_trait_d <- llply(unique(model_summary$trait),
                      function(x) {
                          ggplot(filter(model_summary, effect == 'd', trait == x), aes(loci, mean)) +
                              facet_wrap(~chrom, scale = 'free_x') +
